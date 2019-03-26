3f9983e5c3e0408bcff9946418be728620fd387c
Debnárik Roland <roland.debnarik@gmail.com>
2019-03-26 19:38:52 +0100

    OpenFileDialog

--- DJLF47 - GKLB_INTM038 - Stitching/DJLF47 - GKLB_INTM038 - Stitching/Form1.Designer.cs
+++ DJLF47 - GKLB_INTM038 - Stitching/DJLF47 - GKLB_INTM038 - Stitching/Form1.Designer.cs
@@ -28,13 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.open = new System.Windows.Forms.Button();
+            this.dataGridViewKepek = new System.Windows.Forms.DataGridView();
+            this.View = new System.Windows.Forms.DataGridViewImageColumn();
+            this.FileName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewKepek)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // open
+            // 
+            this.open.Location = new System.Drawing.Point(167, 44);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(75, 23);
+            this.open.TabIndex = 0;
+            this.open.Text = "Open";
+            this.open.UseVisualStyleBackColor = true;
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // dataGridViewKepek
+            // 
+            this.dataGridViewKepek.AllowUserToAddRows = false;
+            this.dataGridViewKepek.AllowUserToDeleteRows = false;
+            this.dataGridViewKepek.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewKepek.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.View,
+            this.FileName});
+            this.dataGridViewKepek.Location = new System.Drawing.Point(34, 117);
+            this.dataGridViewKepek.Name = "dataGridViewKepek";
+            this.dataGridViewKepek.ReadOnly = true;
+            this.dataGridViewKepek.Size = new System.Drawing.Size(240, 150);
+            this.dataGridViewKepek.TabIndex = 1;
+            // 
+            // View
+            // 
+            this.View.HeaderText = "View";
+            this.View.Name = "View";
+            this.View.ReadOnly = true;
+            // 
+            // FileName
+            // 
+            this.FileName.HeaderText = "File Name";
+            this.FileName.Name = "FileName";
+            this.FileName.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.dataGridViewKepek);
+            this.Controls.Add(this.open);
+            this.Name = "Form1";
+            this.Text = "DJLF47 - Stitching";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewKepek)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button open;
+        private System.Windows.Forms.DataGridView dataGridViewKepek;
+        private System.Windows.Forms.DataGridViewImageColumn View;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FileName;
     }
 }
 
--- DJLF47 - GKLB_INTM038 - Stitching/DJLF47 - GKLB_INTM038 - Stitching/Form1.cs
+++ DJLF47 - GKLB_INTM038 - Stitching/DJLF47 - GKLB_INTM038 - Stitching/Form1.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Stitching;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
 
 namespace DJLF47___GKLB_INTM038___Stitching
 {
@@ -15,6 +19,18 @@ namespace DJLF47___GKLB_INTM038___Stitching
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Multiselect = true;
+            dlg.Filter = "jpg files (*.jpg)|*.jpg";
+            if(dlg.ShowDialog()==DialogResult.OK)
+            {
+                dataGridViewKepek.Rows.Clear();
+                Image<Bgr, byte>[] forrasKepek = new Image<Bgr, byte>[dlg.FileNames.Length];
+            }
         }
     }
 }
